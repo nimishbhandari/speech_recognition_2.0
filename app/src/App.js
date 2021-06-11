34edb2b7576dad40ba69220b8ded763df9d7d0ee
Nimish Bhandari <nimishbhandari727521@gmail.com>
2021-06-11 16:03:24 +0530

    setup

---
+++ app/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Speech from "./Speech";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Speech recognition</h1>
+      <Speech />
+    </div>
+  );
+};
+
+export default App;
