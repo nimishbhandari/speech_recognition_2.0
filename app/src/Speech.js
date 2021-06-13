1cff03b3da50381eebc096812ef0b8c2e47be8b1
Nimish Bhandari <nimishbhandari727521@gmail.com>
2021-06-13 20:00:52 +0530

    minor

--- app/src/Speech.js
+++ app/src/Speech.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import SpeechRecognition, {
   useSpeechRecognition,
 } from "react-speech-recognition";
 
 const Speech = () => {
   const { transcript, resetTranscript } = useSpeechRecognition();
+
+  useEffect(() => {
+    {
+      SpeechRecognition.startListening();
+    }
+  });
 
   if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
     return null;
